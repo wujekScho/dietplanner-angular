2f12190fe0884112113be684b6b1ef5db603053e
wujekScho <schodziak@gmail.com>
2019-06-20 22:14:06 +0200

    added view for daymeals

--- src/app/components/planned-meals/planned-meals.component.ts
+++ src/app/components/planned-meals/planned-meals.component.ts
@@ -1,6 +1,7 @@
 import { PlannedDay } from './../../../common/interfaces';
 import { PlannedDaysService } from '../../services/planned-days.service';
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-planned-meals',
@@ -8,16 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planned-meals.component.css']
 })
 export class PlannedMealsComponent implements OnInit {
-  plannedDays: PlannedDay[];
+  plannedDays: PlannedDay[] = [];
+  procededDates: Date[] = [];
+  day;
 
   constructor(private service: PlannedDaysService) { }
 
   ngOnInit() {
     this.service.getAllByUserId(1).subscribe((plannedDays: PlannedDay[]) => this.plannedDays = plannedDays);
+    for (let i = 0; i <= 12; i++) {
+      this.procededDates.push(moment().add(i, 'days').toDate());
+    }
   }
 
-  onClick() {
-    console.log(this.plannedDays);
+  onClick(day: PlannedDay) {
+    console.log(day);
+  }
+
+  getDateOfPlannedDay(day: PlannedDay) {
+    let dayArr = day.day;
+    return new Date(dayArr[0], dayArr[1] - 1, dayArr[2]);
+  }
+
+  getDateOfNow() {
+    let today = new Date();
+    return new Date(today.getFullYear(), today.getMonth(), today.getDate());
+  }
+
+  compareDates(a: Date, b: Date) {
+    return (a.getDate() == b.getDate()) && (a.getMonth() == b.getMonth()) && (a.getFullYear() == b.getFullYear());
+  }
+
+  getPlannedDayObjectOfDate(date: Date) {
+    for (let day of this.plannedDays) {
+      if (this.compareDates(this.getDateOfPlannedDay(day), date)) {
+        return day;
+      }
+    }
+    return null;
+  }
+
+  addDayMeals(date) {
+    console.log(date);
   }
 }
 
